cce6baaa1cff02471fa319a7ce501fc4e0d94455
kavin <lenye01@gmail.com>
2013-03-01 22:06:39 +0800

    Initial Commit

---
+++ Share Me/KVAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  KVAppDelegate.h
+//  Share Me
+//
+//  Created by 孟 智 on 13-3-1.
+//  Copyright (c) 2013年 孟 智. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface KVAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Share Me/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Share Me
+//
+//  Created by 孟 智 on 13-3-1.
+//  Copyright (c) 2013年 孟 智. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "KVAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([KVAppDelegate class]));
+    }
+}
